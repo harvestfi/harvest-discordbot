33d24a3be5c8fd668c0a319a7b7a4ee5899a58a4
Brandon Curtis <brandon.curtis@gmail.com>
2020-09-29 09:50:43 -0700

    add !vault fasset

--- bot.py
+++ bot.py
@@ -22,11 +22,39 @@ UNIROUTER_ADDR = os.getenv("UNIROUTER_ADDR")
 UNIROUTER_ABI = os.getenv("UNIROUTER_ABI")
 UNIPOOL_ADDR= os.getenv("UNIPOOL_ADDR")
 UNIPOOL_ABI = os.getenv("UNIPOOL_ABI")
+VAULT_ABI = os.getenv("VAULT_ABI")
 ONE_18DEC = 1000000000000000000
 
 w3 = Web3(Web3.HTTPProvider(NODE_URL))
 controller_contract = w3.eth.contract(address=UNIROUTER_ADDR, abi=UNIROUTER_ABI)
 pool_contract = w3.eth.contract(address=UNIPOOL_ADDR, abi=UNIPOOL_ABI)
+
+vaults = {
+  '0x8e298734681adbfC41ee5d17FF8B0d6d803e7098': {'asset': 'fWETH', 'decimals': 18,},
+  '0xe85C8581e60D7Cd32Bbfd86303d2A4FA6a951Dac': {'asset': 'fDAI', 'decimals': 18,},
+  '0xc3F7ffb5d5869B3ade9448D094d81B0521e8326f': {'asset': 'fUSDC', 'decimals': 6,},
+  '0xc7EE21406BB581e741FBb8B21f213188433D9f2F': {'asset': 'fUSDT', 'decimals': 6,},
+  '0xF2B223Eb3d2B382Ead8D85f3c1b7eF87c1D35f3A': {'asset': 'FARM yDAI+yUSDC+yUSDT+yTUSD', 'decimals': 18,},
+  '0xfBe122D0ba3c75e1F7C80bd27613c9f35B81FEeC': {'asset': 'fRenBTC', 'decimals': 8,},
+  '0xc07EB91961662D275E2D285BdC21885A4Db136B0': {'asset': 'fWBTC', 'decimals': 8,},
+  '0x192E9d29D43db385063799BC239E772c3b6888F3': {'asset': 'fCRVRenWBTC', 'decimals': 18,},
+  '0xb1FeB6ab4EF7d0f41363Da33868e85EB0f3A57EE': {'asset': 'fUNI-ETH-WBTC', 'decimals': 18,},
+  '0xB19EbFB37A936cCe783142955D39Ca70Aa29D43c': {'asset': 'fUNI-ETH-USDT', 'decimals': 18,},
+  '0x63671425ef4D25Ec2b12C7d05DE855C143f16e3B': {'asset': 'fUNI-ETH-USDC', 'decimals': 18,},
+  '0x1a9F22b4C385f78650E7874d64e442839Dc32327': {'asset': 'fUNI-ETH-DAI', 'decimals': 18,},
+}
+
+
+vault_addr = {
+    'fdai'        : {'addr': '0xe85C8581e60D7Cd32Bbfd86303d2A4FA6a951Dac',},
+    'fusdc'       : {'addr': '0xc3F7ffb5d5869B3ade9448D094d81B0521e8326f',},
+    'fusdt'       : {'addr': '0xc7EE21406BB581e741FBb8B21f213188433D9f2F',},
+    'fwbtc'       : {'addr': '0xc07EB91961662D275E2D285BdC21885A4Db136B0',},
+    'frenbtc'     : {'addr': '0xfBe122D0ba3c75e1F7C80bd27613c9f35B81FEeC',},
+    'fcrvrenwbtc' : {'addr': '0x192E9d29D43db385063799BC239E772c3b6888F3', 'startblock': 10815917},
+    'fweth'       : {'addr': '0x8e298734681adbfC41ee5d17FF8B0d6d803e7098',},
+}
+
 
 client = discord.Client(command_prefix='!')
 activity_start = discord.Streaming(
@@ -59,15 +87,32 @@ async def update_price():
 @client.event
 async def on_message(msg):
     if client.user.id != msg.author.id:
-        if 'foo' in msg.content:
+        if '!foo' in msg.content:
             await msg.channel.send('bar')
-        if 'contribute' in msg.content:
+        if '!bot' in msg.content:
+            embed = discord.Embed(
+                    title='Autonomous Agricultural Assistant, at your service :tractor:',
+                    description=':bar_chart: `!trade`: FARM markets and trading\n'
+                                ':thinking: `!payout`: information on farming rewards\n'
+                                ':globe_with_meridians: `!contribute`: contribute to the community wiki\n'
+                                ':bank: `!vault fdai/fwbtc/etc`: Harvest vault state\n'
+                                ':chart_with_upwards_trend: improve me [on GitHub](https://github.com/brandoncurtis/harvest-discordbot)'
+                    )
+            await msg.channel.send(embed=embed)
+        if '!payout' in msg.content:
+            embed = discord.Embed(
+                    title='When do I get the CRV/SWRV/UNI/&etc? :thinking:',
+                    description='Farmed tokens are sold to grow the value of your deposit :seedling: '
+                                '[read more](https://farm.chainwiki.dev/en/strategy)',
+                    )
+            await msg.channel.send(embed=embed)
+        if '!contribute' in msg.content:
             embed = discord.Embed(
                     title='**:sparkles: Great Idea :sparkles:**',
                     description='please add that [to the wiki](https://farm.chainwiki.dev/en/contribute)!',
                     )
             await msg.channel.send(embed=embed)
-        if 'trade' in msg.content:
+        if '!trade' in msg.content:
             embed = discord.Embed(
                     title='**How To Buy FARM :bar_chart:**',
                     )
@@ -99,7 +144,27 @@ async def on_message(msg):
                     inline = False
                     )
             await msg.channel.send(embed=embed)
-            
+        if '!vault' in msg.content:
+            vault = msg.content.split(' ')[-1].lower()
+            underlying = vault[1:]
+            shareprice, vault_total, vault_buffer = get_vaultstate(vault)
+            vault_invested = vault_total - vault_buffer
+            embed = discord.Embed(
+                    title=f'{vault} Vault State :bank::mag:',
+                    description=f':moneybag: {vault} share price = {shareprice} {underlying}\n'
+                                f':sponge: {underlying} withdrawal buffer = {vault_buffer:,.2f} {underlying}\n'
+                                f':bar_chart: {underlying} invested = {vault_invested:,.2f} {underlying} ({100*vault_invested/vault_total:0.2f}%)\n'
+                    )
+            await msg.channel.send(embed=embed)
+
+def get_vaultstate(vault):
+    vault_contract = w3.eth.contract(address=vault_addr[vault]['addr'], abi=VAULT_ABI)
+    vault_decimals = vault_contract.functions['decimals']().call()
+    vault_shareprice = vault_contract.functions['getPricePerFullShare']().call()*10**(-1*vault_decimals)
+    vault_total = vault_contract.functions['underlyingBalanceWithInvestment']().call()*10**(-1*vault_decimals)
+    vault_buffer = vault_contract.functions['underlyingBalanceInVault']().call()*10**(-1*vault_decimals)
+    return (vault_shareprice, vault_total, vault_buffer)
+
 
 def main():
     client.run(DISCORD_BOT_TOKEN)
